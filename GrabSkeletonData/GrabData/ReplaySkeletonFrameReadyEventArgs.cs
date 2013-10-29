a1d86bdc99e71f495b9914856f5670bcf395f04c
owen2801 <owen12888@yahoo.com.hk>
2013-10-29 20:41:16 +0800

    No longer use

--- GrabSkeletonData/GrabData/ReplaySkeletonFrameReadyEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GrabSkeletonData.GrabData
-{
-    public class ReplaySkeletonFrameReadyEventArgs : EventArgs
-    {
-        public ReplaySkeletonFrame SkeletonFrame { get; set; } // construct a new value called 'SkeletonFrame', and gettable and settable
-    }
-}
